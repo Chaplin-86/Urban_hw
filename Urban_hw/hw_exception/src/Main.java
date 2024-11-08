950eea30081c5fca27762b888b1d9ab1c9da1ddd
Chaplin-86 <162501705+Chaplin-86@users.noreply.github.com>
2024-11-08 16:23:07 +0300

    Add files via upload

---
+++ Urban_hw/hw_exception/src/Main.java
@@ -0,0 +1,48 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        /**
+         * Task 1
+         * Напишите метод, который принимает целое число в качестве параметра и выдает исключение, если число нечетное.
+         * Необходимо протестировать ее в методе main с разными числами.
+         */
+
+//        EvenNumber number = new EvenNumber();
+//        EvenNumber number1 = new EvenNumber();
+//        EvenNumber number2 = new EvenNumber();
+//
+//
+//        System.out.println(number.isEven(4));
+//        System.out.println(number1.isEven(16));
+//        System.out.println(number2.isEven(5));
+
+        /** Task 2
+         * ***Напишите метод, который принимает строку в качестве входных данных и выдает исключение,
+         * если строка содержит числа.
+         */
+
+        CheckString string = new CheckString();
+        String str = "В корзине 5 яблок";
+
+        System.out.println(string.checkNumeric(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
+
