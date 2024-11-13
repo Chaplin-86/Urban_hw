2f0957179231e0580da5439d3ee7dbd01b377d43
Chaplin-86 <162501705+Chaplin-86@users.noreply.github.com>
2024-11-13 17:40:09 +0300

    Update CheckString.java

--- Urban_hw/hw_exception/src/CheckString.java
+++ Urban_hw/hw_exception/src/CheckString.java
@@ -1,18 +1,1 @@
-import java.util.Arrays;
 
-public class CheckString {
-
-    public boolean checkNumeric(String str) throws Exception {
-
-        char[] elements = str.toCharArray();
-        char[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for(char chr : elements) {
-            if(chr) {
-                throw new Exception("Символ является числом");
-            }
-
-        }
-        return true;
-
-    }
-}
