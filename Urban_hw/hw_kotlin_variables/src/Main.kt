9c415285ce82de51e2e161a18b643f99a6a5cb9b
Chaplin-86 <162501705+Chaplin-86@users.noreply.github.com>
2024-12-24 00:13:28 +0300

    Add files via upload

---
+++ Urban_hw/hw_kotlin_variables/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   val age = 38
+    val name = "Maria"
+    val isStudent = true
+    val salary = 60000.0
+    println("Age: " + age + " | " + "Name: " + name + " | " + "Student: " +  isStudent + " | " + "Salary: " + salary)
+}
